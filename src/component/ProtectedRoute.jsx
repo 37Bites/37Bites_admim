02e12493cd64df0b93771a4b593442b9603533e6
Shauryaarajput <planixos8@gmail.com>
2026-02-27 14:10:54 +0530

    check

--- src/component/ProtectedRoute.jsx
+++ src/component/ProtectedRoute.jsx
@@ -1,16 +1,16 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { useSelector } from "react-redux";
+// import { Navigate, Outlet } from "react-router-dom";
+// import { useSelector } from "react-redux";
 
-const ProtectedRoute = ({ allowedRoles }) => {
-  const { isAuthenticated, user } = useSelector((state) => state.auth);
+// const ProtectedRoute = ({ allowedRoles }) => {
+//   const { isAuthenticated, user } = useSelector((state) => state.auth);
 
-  if (!isAuthenticated || !user) {
-    return <Navigate to="/" />;
-  }
+//   if (!isAuthenticated || !user) {
+//     return <Navigate to="/" />;
+//   }
 
-  if (!allowedRoles.includes(user.role)) {
-    return <Navigate to="/unauthorized" />;
-  }
+//   if (!allowedRoles.includes(user.role)) {
+//     return <Navigate to="/unauthorized" />;
+//   }
 
-  return <Outlet />;
-};
+//   return <Outlet />;
+// };
